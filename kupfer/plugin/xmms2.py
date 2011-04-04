b9012919424f3ac035344367f0835590b4d9d98e
Curious Leo <curiousleo@ymail.com>
2011-04-04 14:44:53 +0200

    Merged xmms2_support.py into xmms2.py

--- kupfer/plugin/xmms2.py
+++ kupfer/plugin/xmms2.py
@@ -17,6 +17,7 @@ __author__ = "Leonhard Markert <curiousleo@ymail.com>"
 import itertools
 import gio
 import urllib
+import sqlite3
 from os import path as os_path
 
 try:
@@ -31,7 +32,6 @@ from kupfer.objects import (Leaf, Source, AppLeaf, Action, RunnableLeaf,
 from kupfer import objects, icons, utils, config
 from kupfer.obj.apps import AppLeafContentMixin
 from kupfer import plugin_support
-from kupfer.plugin import xmms2_support
 
 from time import sleep # FIXME: Needed for ugly hack
 
@@ -423,14 +423,14 @@ class XMMS2Source (AppLeafContentMixin, Source):
 		Source.__init__(self, _("XMMS2"))
 	def get_items(self):
 		try:
-			dbfile = xmms2_support.get_xmms2_dbfile()
-			if dbfile: songs = list(xmms2_support.get_xmms2_songs(dbfile))
+			dbfile = get_xmms2_dbfile()
+			if dbfile: songs = list(get_xmms2_songs(dbfile))
 		except StandardError, e:
 			self.output_error(e)
 			songs = []
 
-		albums = xmms2_support.parse_xmms2_albums(songs)
-		artists = xmms2_support.parse_xmms2_artists(songs)
+		albums = parse_xmms2_albums(songs)
+		artists = parse_xmms2_artists(songs)
 		yield Play()
 		yield Pause()
 		yield Next()
@@ -462,3 +462,103 @@ class XMMS2Source (AppLeafContentMixin, Source):
 		yield RunnableLeaf
 		yield SourceLeaf
 		yield SongLeaf
+
+# Copied from xmms2_support.py
+
+NEEDED_KEYS= ("id", "title", "artist", "album", "tracknr", "url")
+
+def _tourl(rawurl):
+	return urllib.unquote_plus(rawurl).encode('latin1').decode('utf-8')
+
+def get_xmms2_dbfile():
+	"""Returns default path of xmms2 media library if it exists"""
+	dbfile = os_path.expanduser("~/.config/xmms2/medialib.db")
+	if os_path.exists(dbfile):
+		return dbfile
+
+def get_xmms2_songs(dbfile):
+	"""Get songs from xmms2 media library (sqlite). Generator function."""
+	db = sqlite3.connect(dbfile)
+	cu = db.execute("""
+			SELECT A.id, A.value,    B.value,           C.value,          D.value,            E.value
+			FROM   Media A,          Media B,           Media C,          Media D,            Media E
+			WHERE  A.key="title" AND B.key="artist" AND C.key="album" AND D.key="tracknr" AND E.key="url"
+			AND    A.id = B.id AND B.id = C.id AND C.id = D.id AND D.id = E.id
+	""")
+
+	for row in cu:
+		# NEEDED_KEYS and returned rows must have the same order for this to work
+		song = dict(zip((NEEDED_KEYS), row))
+		# URLs are saved in quoted format in the db; they're also latin1 encoded but returned as unicode
+		song["url"] = _tourl(song["url"])
+		# Generator
+		yield song
+
+	db.close()
+
+def sort_album(album):
+	"""Sort album in track order"""
+	def get_track_number(rec):
+		try:
+			tnr = int(rec["tracknr"])
+		except (KeyError, ValueError):
+			tnr = 0
+		return tnr
+	album.sort(key=get_track_number)
+
+def sort_album_order(songs):
+	"""Sort songs in order by album then by track number
+
+	>>> songs = [
+	... {"title": "a", "album": "B", "tracknr": "2"},
+	... {"title": "b", "album": "A", "tracknr": "1"},
+	... {"title": "c", "album": "B", "tracknr": "1"},
+	... ]
+	>>> sort_album_order(songs)
+	>>> [s["title"] for s in songs]
+	['b', 'c', 'a']
+	"""
+	pass
+
+	def get_album_order(rec):
+		try:
+			tnr = int(rec["tracknr"])
+		except (KeyError, ValueError):
+			tnr = 0
+		return (rec["album"], tnr)
+	songs.sort(key=get_album_order)
+
+def parse_xmms2_albums(songs):
+	albums = {}
+	for song in songs:
+		song_artist = song["artist"]
+		if not song_artist:
+			continue
+		song_album = song["album"]
+		if not song_album:
+			continue
+		album = albums.get(song_album, [])
+		album.append(song)
+		albums[song_album] = album
+	# sort album in track order
+	for album in albums:
+		sort_album(albums[album])
+	return albums
+
+def parse_xmms2_artists(songs):
+	artists = {}
+	for song in songs:
+		song_artist = song["artist"]
+		if not song_artist:
+			continue
+		artist = artists.get(song_artist, [])
+		artist.append(song)
+		artists[song_artist] = artist
+	# sort in album + track order
+	for artist in artists:
+		sort_album_order(artists[artist])
+	return artists
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
